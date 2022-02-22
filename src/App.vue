503508946512fbdd52e6043a7b5b731632f48bd7
AA12GQ <875151567@qq.com>
2022-02-22 16:31:30 +0800

    初始化并梳理项目的结构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <p>App 根组件</p>
-
+    <div>
+      <i class="toutiao toutiaowode"></i>
+      <i class="toutiao toutiaoshouji"></i>
+      <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
 </template>
 
