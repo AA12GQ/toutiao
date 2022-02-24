d3f0f2395644d9e29745d0662a1a7ffc9652eaac
AA12GQ <875151567@qq.com>
2022-02-24 14:52:49 +0800

    登录注册-封装本地存储模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
 
 Vue.use(Vuex)
 
@@ -7,13 +8,13 @@ const TOKEN_KEY = 'TOUTIAO_USER'
 
 export default new Vuex.Store({
   state: {
-    user: JSON.parse(window.localStorage.getItem(TOKEN_KEY))
+    user: getItem(TOKEN_KEY)
   },
   mutations: {
     setUser(state, data) {
       state.user = data
 
-      window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      setItem(TOKEN_KEY, state.user)
     }
   },
   actions: {
