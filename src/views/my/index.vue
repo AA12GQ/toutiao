5670f9587fc86ed14de432b4158fd30e2fbc8c35
AA12GQ <875151567@qq.com>
2022-02-24 20:31:17 +0800

    个人中心-顶部未登录和已登录状态

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -6,6 +6,37 @@
         <span class="text">登录 / 注册</span>
       </div>
     </div>
+
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+          <van-image class="avatar" round fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+          <span class="name">AA12</span>
+        </div>
+        <div class="right">
+          <van-button size="mini" round >编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data-stats">
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">获赞</span>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -51,6 +82,51 @@ export default {
     .text {
       font-size: 28px;
       color: #fff;
+    }
+  }
+
+  .user-info {
+    .base-info {
+      height: 231px;
+      padding : 76px 32px 23px;
+      box-sizing: border-box;
+      display:flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .avatar {
+        width: 132px;
+        height: 132px;
+        margin-right:23px;
+        border: 4px solid #fff;
+      }
+      .name {
+        font-size:30px;
+        color: #fff;
+      }
+      }
+    }
+    .data-stats{
+      // height: 130px;
+      display: flex;
+      .data-item {
+        height: 130px;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        .count {
+          font-size: 36px;
+        }
+        .text {
+          font-size:23px;
+        }
+      }
     }
   }
 }
