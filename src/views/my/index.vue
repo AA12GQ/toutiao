739ba861f3dc4bd78eb8ed51a855a84617af0838
AA12GQ <875151567@qq.com>
2022-02-24 20:47:59 +0800

    个人中心-宫格导航

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -37,6 +37,16 @@
       </div>
     </div>
 
+    <van-grid :column-num="2" class="grid-nav" clickable>
+      <van-grid-item class="grid-item">
+        <i slot="icon" class="toutiao toutiao-shoucang"></i>
+        <span slot="text" class="text">收藏</span>
+      </van-grid-item>
+      <van-grid-item class="grid-item">
+        <i slot="icon" class="toutiao toutiao-lishi"></i>
+        <span slot="text" class="text">历史</span>
+      </van-grid-item>
+  </van-grid>
   </div>
 </template>
 
@@ -129,5 +139,23 @@ export default {
       }
     }
   }
+
+.grid-nav {
+  .grid-item {
+    height: 141px;
+    i.toutiao {
+      font-size:45px;
+    }
+    .toutiao-shoucang {
+      color: #eb5253;
+    }
+    .toutiao-lishi {
+      color: #ff9d1d;
+    }
+    span.text {
+      font-size: 28px;
+    }
+  }
+}
 }
 </style>
