6fd41e6f110322c4f58ff0acb9f858a34bb85f9f
AA12GQ <875151567@qq.com>
2022-02-24 18:21:24 +0800

    个人中心-页面布局

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="my-container">我的页面</div>
+  <div class="my-container">
+    <div class="header not-login" @click="$router.push('/login')">
+      <div class="login-btn">
+        <img class="mobile-img" src="~@/assets/mobile.png" alt="">
+        <span class="text">登录 / 注册</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,4 +25,33 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.my-container {
+  .header {
+    height: 361px;
+    background:url('~@/assets/banner.png');
+    background-size : cover;
+  }
+  .not-login {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    .login-btn{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    }
+    .mobile-img {
+      width: 132px;
+      height: 132px;
+      margin-bottom:15px;
+    }
+    .text {
+      font-size: 28px;
+      color: #fff;
+    }
+  }
+}
+</style>
