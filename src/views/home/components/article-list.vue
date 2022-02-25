70e42a6a9466091d95ba16523ba1769d0a7cf3d7
AA12GQ <875151567@qq.com>
2022-02-25 17:55:28 +0800

    首页-展示文章列表-下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="article-list">
+  <van-pull-refresh
+    v-model="isRefreshLoading"
+    @refresh="onRefresh"
+    :success-text="refreshSuccessText"
+    :success-duration="1500"
+   >
   <van-list
     v-model="loading"
     :finished="finished"
@@ -10,6 +16,7 @@
     >
   <van-cell v-for="(article, index) in list" :key="index" :title="article.title" />
 </van-list>
+</van-pull-refresh>
   </div>
 </template>
 
@@ -30,7 +37,9 @@ export default {
       loading: false,
       finished: false,
       timestamp: null,
-      error: false
+      error: false,
+      isRefreshLoading: false,
+      refreshSuccessText: ''
     }
   },
   computed: {},
@@ -45,9 +54,10 @@ export default {
           timestamp: this.timestamp || Date.now(),
           with_top: 1
         })
-        if (Math.random() > 0.5) {
-          JSON.parse('daojfhgka')
-        }
+        // 模拟请求失败
+        // if (Math.random() > 0.5) {
+        //   JSON.parse('daojfhgka')
+        // }
         const { results } = data.data
         this.list.push(...results)
         this.loading = false
@@ -60,6 +70,26 @@ export default {
       } catch (err) {
         this.error = true
         this.loading = false
+      }
+    },
+    async onRefresh() {
+      try {
+        const { data } = await getArticles({
+          channel_id: this.channel.id,
+          timestamp: Date.now(),
+          with_top: 1
+        })
+        // 模拟请求失败
+        // if (Math.random() > 0.2) {
+        //   JSON.parse('daojfhgka')
+        // }
+        const { results } = data.data
+        this.list.unshift(...results)
+        this.isRefreshLoading = false
+        this.refreshSuccessText = `刷新成功，更新了${results.length}条数据`
+      } catch (err) {
+        this.refreshSuccessText = '刷新失败'
+        this.isRefreshLoading = false
       }
     }
   }
