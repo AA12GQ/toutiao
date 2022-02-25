57ca111fe97a00bc9bbe3441948d3baaa1b6bd55
AA12GQ <875151567@qq.com>
2022-02-25 13:35:43 +0800

    首页-处理文章频道列表-样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -11,10 +11,17 @@
       >搜索</van-button>
     </van-nav-bar>
     <van-tabs v-model="active" swipeable animated class="channel-tabs">
-    <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 1">内容 1</van-tab>
       <van-tab title="标签 2">内容 2</van-tab>
       <van-tab title="标签 3">内容 3</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 2">内容 5</van-tab>
+      <van-tab title="标签 3">内容 6</van-tab>
+      <van-tab title="标签 4">内容 7</van-tab>
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="toutiao toutiao-gengduo"></i>
+      </div>
     </van-tabs>
   </div>
 </template>
@@ -41,6 +48,11 @@ export default {
 .home-container {
  /deep/ .van-nav-bar__title {
     max-width: unset;
+  }
+  .placeholder {
+    flex-shrink: 0;
+    width: 66px;
+    height: 8px;
   }
   .search-btn {
     width: 555px;
@@ -74,6 +86,29 @@ export default {
     height: 6px;
     background-color: #3296fa;
   }
+  .hamburger-btn {
+    position: fixed;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 66px;
+    height: 82px;
+    background-color: #fff;
+    opacity: 0.902;
+    i.toutiao {
+      font-size: 33px;
+    }
+    &:before {
+      position: absolute;
+      left: 0;
+      content : '';
+      width: 1px;
+      height: 100%;
+      background-image: url(~@/assets/gradient-gray-line.png);
+      background-size: contain;
+    }
+    }
   }
 }
 </style>
