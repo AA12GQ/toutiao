25a6078054bc3dadeb322fc6bc66c3ec365acfc8
AA12GQ <875151567@qq.com>
2022-02-28 21:42:07 +0800

    首页-文章列表项-处理相对时间(处理完成)

--- src/utils/dayjs.js
+++ src/utils/dayjs.js
@@ -1,7 +1,14 @@
+import Vue from 'vue'
 import dayjs from 'dayjs'
 
-require('dayjs/locale/zh-cn')
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
+
+dayjs.extend(relativeTime)
 
 dayjs.locale('zh-cn') // 全局使用\
 
-console.log(dayjs().format('YYYY-MM-DD'))
+Vue.filter('relativeTime', value => {
+  return dayjs().to(dayjs(value))
+})
+// console.log(dayjs().format('YYYY-MM-DD'))
